4cba68e4b6c8775f28093d123d324f604f16481d
tarpit230 <118742104+tarpit230@users.noreply.github.com>
2026-07-06 22:36:00 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++){
+            if(mp.find(nums[i]) != mp.end()) return true;
+            mp[nums[i]]++;
+        }
+        return false;
+    }
+};
